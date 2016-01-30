99606d644ebd7c2aeb836a27d9e0c07918bb29d4
panickylemon <alenka__1992@mail.ru>
2016-01-30 13:33:46 +0300

    3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-//$( "#burger_menu" ).click(function() {
-//    $("#burger_menu").hide()
-//});
-
 $(document).ready(function(){
-    $("#burger_menu").click(function(){
-        $(this).find("#burger_menu").hide();
-    })
+
+    $(".burger_menu").click(function() {
+        $(".nav_mobile").slideToggle("slow");
+        $(".burger_menu").hide();
+        $(".burger_menu_close").show();
+    });
+    $(".burger_menu_close").click(function() {
+        $(".nav_mobile").slideToggle("slow");
+        $(".burger_menu").show();
+        $(".burger_menu_close").hide();
+    });
 });
 
-$("div").click(function () {
-    $("div").slideToggle("slow");
-});
 
